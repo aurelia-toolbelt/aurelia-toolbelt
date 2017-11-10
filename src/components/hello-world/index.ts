283a303dcdf722db1aafaf90cc53a108a28dcb5e
Saeed Ganji <shahabganji@gmail.com>
2017-11-10 20:04:20 +0330

    remove(hello-world)

--- src/components/hello-world/index.ts
+++
@@ -1,9 +0,0 @@
-import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
-
-
-
-export * from './hello-world';
-
-export function configure(config: FrameworkConfiguration) {
-    config.globalResources( PLATFORM.moduleName('./hello-world') );
-}
