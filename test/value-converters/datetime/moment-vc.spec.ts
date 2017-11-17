3c47c0c2ebcdc7099ae96e26b4671d9692e039b1
Saeed Ganji <shahabganji@gmail.com>
2017-11-17 16:04:20 +0330

    test(age value converter)

--- test/value-converters/datetime/moment-vc.spec.ts
+++ test/value-converters/datetime/moment-vc.spec.ts
@@ -1,6 +1,9 @@
 
 import { DateValueConverter, AgeValueConverter, TimeConverter, RelativeValueConverter } from '../../../src/value-converters/datetime/moment-vc';
 
+import * as jMoment from 'jalali-moment';
+
+
 describe('monet value converter', () => {
 
   it('date of birth', () => {
@@ -15,4 +18,19 @@ describe('monet value converter', () => {
 
   });
 
+  it('date of birth in persian', () => {
+
+    let ageConvert: AgeValueConverter = new AgeValueConverter();
+
+    let result = ageConvert.toView('1363/11/03');
+    let expected = jMoment().year() - 1363;
+
+    expect(result).toBe(expected);
+
+  });
+
 });
+
+
+// tslint:disable-next-line:no-empty
+xdescribe('time check in different formats', () => { });
