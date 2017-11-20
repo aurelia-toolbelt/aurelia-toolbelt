87ae5b278dc1f6e9489c7be39b3875301e515b52
Saeed Ganji <shahabganji@gmail.com>
2017-11-20 09:46:50 +0330

    revert( checkbox ) && add( radio )

--- src/components/aurelia-checkbox/index.ts
+++ src/components/aurelia-checkbox/index.ts
@@ -3,7 +3,11 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 
 export * from './elements/pretty-checkbox';
+export * from './elements/pretty-radio';
 
 export function configure(config: FrameworkConfiguration) {
-  config.globalResources(PLATFORM.moduleName('./elements/pretty-checkbox'));
+  config.globalResources([
+    PLATFORM.moduleName('./elements/pretty-checkbox'),
+    PLATFORM.moduleName('./elements/pretty-radio')
+  ]);
 }
