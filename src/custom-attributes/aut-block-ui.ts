18893600254466b251897622e20c8d8e0a051b35
Hamed <hamedfathi@outlook.com>
2017-11-20 21:00:41 +0330

    init(aut-block-ui with 9 overlay themes)

--- src/custom-attributes/aut-block-ui.ts
+++ src/custom-attributes/aut-block-ui.ts
@@ -1,9 +1,7 @@
 
 import { noView, customAttribute, inject, bindable, bindingMode } from 'aurelia-framework';
 
-import './aut-block-ui.css';
-
-@noView(['./aut-block-ui.css'])
+@noView()
 @customAttribute('aut-block-ui')
 @inject(Element)
 export class BlockUI {
@@ -17,8 +15,7 @@ export class BlockUI {
     }
 
     private attached() {
-        this.blocker.innerText = 'I am blocking you';
-
+        // this.blocker.innerText = 'I am blocking you';
         // The calculations must be done before appendChild(this.blocker)
         const left = this.element.getBoundingClientRect().left;
         const top = this.element.getBoundingClientRect().top;
@@ -27,8 +24,259 @@ export class BlockUI {
 
         this.element.appendChild(this.blocker);
 
-        this.blocker.style.cssText = `position: fixed;display: none;width: ${width}px;height: ${height}px;top: ${top}px;left: ${left}px;
-        background-color: rgba(0, 0, 0, 0.5);z-index: 9999999;cursor: pointer;`;
+        const overlayStyle = `
+        .overlay01 {
+          position: fixed;
+          top: ${top}px;
+          left: ${left}px;
+          z-index: 999999;
+          width: ${width}px;
+          height: ${height}px;
+          background: rgba(0, 0, 0, .7);
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+        }
+        .overlay01.is-open {
+          opacity: 1;
+          visibility: visible;
+        }
+
+        .overlay02 {
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 999999;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, .7);
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+          transform: scale(.2);
+        }
+        .overlay02.is-open {
+          opacity: 1;
+          visibility: visible;
+          transform: scale(1);
+        }
+
+        .overlay03 {
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 999999;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, .7);
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+          transform: scaleY(0);
+        }
+        .overlay03.is-open {
+          opacity: 1;
+          visibility: visible;
+          transform: scaleY(1);
+        }
+
+        .overlay04 {
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 999999;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, .7);
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+          transform: translateY(-100%);
+        }
+        .overlay04.is-open {
+          opacity: 1;
+          visibility: visible;
+          transform: translateY(0);
+        }
+
+        .overlay05 {
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 999999;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, .7);
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+          transform: translateX(-100%);
+        }
+        .overlay05.is-open {
+          opacity: 1;
+          visibility: visible;
+          transform: translateX(0);
+        }
+
+        .overlay06 {
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 999999;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, .7);
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+          animation: .3s linear overlay06-close;
+        }
+        .overlay06.is-open {
+          opacity: 1;
+          visibility: visible;
+          animation: .3s linear overlay06-open;
+        }
+        @keyframes overlay06-open {
+          0% {
+            transform: scaleY(0);
+            transform-origin: top;
+          }
+          100% {
+            transform: scaleY(1);
+            transform-origin: top;
+          }
+        }
+        @keyframes overlay06-close {
+          0% {
+            transform: scaleY(1);
+            transform-origin: bottom;
+          }
+          100% {
+            transform: scaleY(0);
+            transform-origin: bottom;
+          }
+        }
+
+        .overlay07 {
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 999999;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, .7);
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+          animation: .3s linear overlay07-close;
+        }
+        .overlay07.is-open {
+          opacity: 1;
+          visibility: visible;
+          animation: .3s linear overlay07-open;
+        }
+        @keyframes overlay07-open {
+          0% {
+            transform: scaleX(0);
+            transform-origin: left;
+          }
+          100% {
+            transform: scaleX(1);
+            transform-origin: left;
+          }
+        }
+        @keyframes overlay07-close {
+          0% {
+            transform: scaleX(1);
+            transform-origin: right;
+          }
+          100% {
+            transform: scaleX(0);
+            transform-origin: right;
+          }
+        }
+
+        .overlay08,
+        .overlay08::before,
+        .overlay08::after {
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 999999;
+          width: 100%;
+          height: 100%;
+          transition: .3s linear;
+        }
+        .overlay08 {
+          opacity: 0;
+          visibility: hidden;
+        }
+        .overlay08::before,
+        .overlay08::after {
+          content: '';
+          background: rgba(0, 0, 0, .45);
+        }
+        .overlay08::before {
+          transform: translateY(-100%);
+        }
+        .overlay08::after {
+          transform: translateY(100%);
+        }
+        .overlay08.is-open {
+          opacity: 1;
+          visibility: visible;
+        }
+        .overlay08.is-open::before,
+        .overlay08.is-open::after {
+          transform: translateY(0);
+        }
+
+        .overlay09,
+        .overlay09::before,
+        .overlay09::after {
+          position: fixed;
+          top: 0;
+          z-index: 999999;
+          height: 100%;
+        }
+        .overlay09 {
+          left: 0;
+          width: 100%;
+          opacity: 0;
+          visibility: hidden;
+          transition: .3s linear;
+        }
+        .overlay09::before,
+        .overlay09::after {
+          left: 50%;
+          content: '';
+          width: 200%;
+          margin-left: -100%;
+          background: rgba(0, 0, 0, .45);
+          transform: skewX(-30deg) translateX(-100%);
+          transition: transform .3s linear .3s;
+        }
+        .overlay09.is-open {
+          opacity: 1;
+          visibility: visible;
+        }
+        .overlay09.is-open::before {
+          transform: skewX(-30deg) translateX(0);
+          transition: transform .3s linear;
+        }
+        .overlay09.is-open::after {
+          transform: skewX(-30deg) translateX(0);
+          transition: transform .3s linear .15s;
+        }`;
+
+        const style = document.createElement('style');
+        style.textContent = overlayStyle;
+        document.head.appendChild(style);
+
+
+
+        /*this.blocker.style.cssText = `position: fixed;display: none;width: ${width}px;height: ${height}px;top: ${top}px;left: ${left}px;
+        background-color: rgba(0, 0, 0, 0.5);z-index: 9999999;cursor: pointer;`;*/
     }
 
 
@@ -37,13 +285,16 @@ export class BlockUI {
         console.log(`do blocking: ${doBlocking}`);
 
         if (doBlocking) {
-            this.blocker.style.display = 'block';
+            /*this.blocker.style.display = 'block';*/
+            this.blocker.classList.add('overlay', 'overlay01', 'is-open');
             // this.element.appendChild(this.blocker);
             return;
         }
 
         if (this.blocker.parentElement === this.element) {
-            this.blocker.style.display = 'none';
+            this.blocker.classList.remove('is-open');
+
+            /*this.blocker.style.display = 'none';*/
             // this.element.removeChild(this.blocker);
         }
 
