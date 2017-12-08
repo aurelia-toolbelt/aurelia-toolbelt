66ea4287f73991a29bfcb45ec8f5979662ce31bc
Saeed Ganji <shahabganji@gmail.com>
2017-12-08 22:24:07 +0330

    init( bootstrap 4 )

--- sample/src/extra.ts
+++ sample/src/extra.ts
@@ -7,10 +7,6 @@ import 'pretty-checkbox/dist/pretty-checkbox.css';
 import 'bootstrap-float-label/dist/bootstrap-float-label.css';
 import 'bootstrap-toggle';
 import 'bootstrap-toggle/css/bootstrap-toggle.css';
-import 'highlight.js/styles/vs.css';
-import 'highlight.js/styles/monokai.css';
-import 'highlight.js/styles/ocean.css';
-import 'highlight.js/styles/tomorrow-night-blue.css';
 import 'highlight.js/styles/dracula.css';
 
 let x = fuse;
