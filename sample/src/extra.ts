fcfed1ed0520dbeb002913f06d7c74e916b84492
Saeed Ganji <shahabganji@gmail.com>
2017-12-09 14:30:37 +0330

    update(fonts)

--- sample/src/extra.ts
+++ sample/src/extra.ts
@@ -7,11 +7,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 /* Font-Awesome */
 import 'font-awesome/css/font-awesome.css';
-import 'font-awesome/fonts/fontawesome-webfont.eot';
-import 'font-awesome/fonts/fontawesome-webfont.svg';
-import 'font-awesome/fonts/fontawesome-webfont.ttf';
-import 'font-awesome/fonts/fontawesome-webfont.woff';
-import 'font-awesome/fonts/fontawesome-webfont.woff2';
+// import 'font-awesome/fonts/fontawesome-webfont.eot';
+// import 'font-awesome/fonts/fontawesome-webfont.svg';
+// import 'font-awesome/fonts/fontawesome-webfont.ttf';
+// import 'font-awesome/fonts/fontawesome-webfont.woff';
+// import 'font-awesome/fonts/fontawesome-webfont.woff2';
 /** ***************************************************** */
 
 import 'pretty-checkbox/dist/pretty-checkbox.css';
