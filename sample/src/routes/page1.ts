cc48cc824123cd78890ae797baf11305843c93c6
Saeed Ganji <shahabganji@gmail.com>
2017-12-11 13:40:46 +0330

    add(blockui to mark down-editor)

--- sample/src/routes/page1.ts
+++ sample/src/routes/page1.ts
@@ -6,36 +6,36 @@ import { HttpClient } from 'aurelia-http-client';
 @autoinject
 export class Page1 {
 
-    public title: string;
+  public title: string;
 
-    public rate = 3;
+  public rate = 3;
 
-    public myDisable = true;
-
-
-    private blockValue1 = false;
-    private blockValue2 = false;
-    private blockValue3 = false;
+  public myDisable = true;
 
 
-    constructor(public router: Router, private http: HttpClient) {
-        // todo
-    }
-    private toggleBlock1() {
-        this.blockValue1 = !this.blockValue1;
-    }
-    private toggleBlock2() {
-        this.blockValue2 = !this.blockValue2;
-    }
-    private toggleBlock3() {
-        this.blockValue3 = !this.blockValue3;
-    }
-    private canActivate(a, b, c) {
-        this.title = b.title;
-    }
+  private blockValue1 = true;
+  private blockValue2 = true;
+  private blockValue3 = true;
 
-    private buttonClicked(event: Event, target: HTMLButtonElement) {
-       return  this.http.get('https://github.com');
-    }
+
+  constructor(public router: Router, private http: HttpClient) {
+    // todo
+  }
+  private toggleBlock1() {
+    this.blockValue1 = !this.blockValue1;
+  }
+  private toggleBlock2() {
+    this.blockValue2 = !this.blockValue2;
+  }
+  private toggleBlock3() {
+    this.blockValue3 = !this.blockValue3;
+  }
+  private canActivate(a, b, c) {
+    this.title = b.title;
+  }
+
+  private buttonClicked(event: Event, target: HTMLButtonElement) {
+    return this.http.get('https://github.com');
+  }
 
 }
