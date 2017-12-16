2bbce09f6aad42578d10a06ca1a38bdc274af071
Hamed <hamedfathi@outlook.com>
2017-12-16 21:52:13 +0330

    add(aut-block-ui options)

--- src/components/jquery/block-ui/aut-block-ui.ts
+++ src/components/jquery/block-ui/aut-block-ui.ts
@@ -17,47 +17,64 @@ export class JQueryBlockUI {
     @bindable({ defaultBindingMode: bindingMode.oneWay }) public spinnerSize: string;
 
     private content: HTMLDivElement;
-
     constructor(private element: Element, private option: IAutBlockUIOptions) {
 
     }
-    /*private bind() {
-        $.blockUI.defaults.allowBodyStretch = this.option.allowBodyStretch;
-        $.blockUI.defaults.draggable = this.option.draggable;
-        $.blockUI.defaults.css = this.option.css;
-        $.blockUI.defaults.overlayCSS = this.option.overlayCSS;
-        $.blockUI.defaults.cursorReset = this.option.cursorReset;
-        $.blockUI.defaults.iframeSrc = this.option.iframeSrc;
-        $.blockUI.defaults.forceIframe = this.option.forceIframe;
-        $.blockUI.defaults.baseZ = this.option.baseZ;
-        $.blockUI.defaults.centerX = this.option.centerX;
-        $.blockUI.defaults.bindEvents = this.option.bindEvents;
-        $.blockUI.defaults.constrainTabKey = this.option.constrainTabKey;
-        $.blockUI.defaults.fadeIn = this.option.fadeIn;
-        $.blockUI.defaults.fadeOut = this.option.fadeOut;
-        $.blockUI.defaults.timeout = this.option.timeout;
-        $.blockUI.defaults.showOverlay = this.option.showOverlay;
-        $.blockUI.defaults.focusInput = this.option.focusInput;
-        $.blockUI.defaults.onBlock = this.option.onBlock;
-        $.blockUI.defaults.onUnblock = this.option.onUnblock;
-        $.blockUI.defaults.quirksmodeOffsetHack = this.option.quirksmodeOffsetHack;
-        $.blockUI.defaults.allowBodyStretch = this.option.allowBodyStretch;
-        $.blockUI.defaults.blockMsgClass = this.option.blockMsgClass;
-        $.blockUI.defaults.ignoreIfBlocked = this.option.ignoreIfBlocked;
-    }*/
-
-
     private attached() {
+        $.blockUI.defaults.allowBodyStretch = this.option.allowBodyStretch || true;
+        $.blockUI.defaults.draggable = this.option.draggable || true;
+        $.blockUI.defaults.css = this.option.css || {
+            padding: 0,
+            margin: 0,
+            width: '30%',
+            top: '40%',
+            left: '35%',
+            textAlign: 'center',
+            color: '#000',
+            border: '3px solid #aaa',
+            backgroundColor: '#fff',
+            cursor: 'wait'
+        };
+        $.blockUI.defaults.overlayCSS = this.option.overlayCSS || {
+            backgroundColor: '#000',
+            opacity: 0.6,
+            cursor: 'wait'
+        };
+        $.blockUI.defaults.cursorReset = this.option.cursorReset || 'default';
+        $.blockUI.defaults.iframeSrc = this.option.iframeSrc || (/^https/i.test(window.location.href || '') ? 'javascript:false' : 'about:blank');
+        $.blockUI.defaults.forceIframe = this.option.forceIframe || false;
+        $.blockUI.defaults.baseZ = this.option.baseZ || 1000;
+        $.blockUI.defaults.centerX = this.option.centerX || true;
+        $.blockUI.defaults.centerY = this.option.centerY || true;
+        $.blockUI.defaults.bindEvents = this.option.bindEvents || true;
+        $.blockUI.defaults.constrainTabKey = this.option.constrainTabKey || true;
+        $.blockUI.defaults.fadeIn = this.option.fadeIn || 200;
+        $.blockUI.defaults.fadeOut = this.option.fadeOut || 400;
+        $.blockUI.defaults.timeout = this.option.timeout || 0;
+        $.blockUI.defaults.showOverlay = this.option.showOverlay || true;
+        $.blockUI.defaults.focusInput = this.option.focusInput || true;
+        $.blockUI.defaults.onBlock = this.option.onBlock || null;
+        $.blockUI.defaults.onUnblock = this.option.onUnblock || null;
+        $.blockUI.defaults.quirksmodeOffsetHack = this.option.quirksmodeOffsetHack || 4;
+        $.blockUI.defaults.blockMsgClass = this.option.blockMsgClass || 'blockMsg';
+        $.blockUI.defaults.ignoreIfBlocked = this.option.ignoreIfBlocked || false;
+
         this.blockChanged(this.block);
+
     }
 
     private blockChanged(isBlocked: boolean | string) {
+        let option = {
+            message: this.message
+        };
         if (isBlocked) {
-            $(this.content).block({
-                title: this.title,
-                message: this.message
-            });
+            $(this.content).block(option);
             this.element.classList.add('block-ui-content');
+            $(window).resize(() => {
+                if (this.element.classList.contains('block-ui-content')) {
+                    $(this.content).block(option);
+                }
+            });
         } else {
             $(this.content).unblock();
             this.element.classList.remove('block-ui-content');
