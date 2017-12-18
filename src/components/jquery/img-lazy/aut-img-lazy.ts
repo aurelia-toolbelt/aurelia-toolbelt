4c0ea748280025e3be55867471c2bac8abc852f6
Hamed Fathi <noeffect@outlook.com>
2017-12-18 15:29:54 +0330

    update(aut-img-lazy)

--- src/components/jquery/img-lazy/aut-img-lazy.ts
+++ src/components/jquery/img-lazy/aut-img-lazy.ts
@@ -1,7 +1,17 @@
-import { transient, customElement, inject, containerless, bindable, bindingMode, observable, DOM } from 'aurelia-framework';
+import { customElement, inject, bindable, bindingMode, observable, DOM } from 'aurelia-framework';
 import * as $ from 'jquery';
 
 @customElement('aut-img-lazy')
 export class JQueryLazy {
+
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public url: string;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public beforeLoad: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public afterLoad: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public onError: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public onFinishedAll: Function;
+
+
+
+
 
 }
