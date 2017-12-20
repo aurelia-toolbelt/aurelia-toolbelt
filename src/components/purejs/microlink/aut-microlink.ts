9d1a89e4f4cbb44c8cdfc299ae2b9b4b1e8b1b78
Saeed Ganji <shahabganji@gmail.com>
2017-12-20 10:10:21 +0330

    remove(console)

--- src/components/purejs/microlink/aut-microlink.ts
+++ src/components/purejs/microlink/aut-microlink.ts
@@ -6,8 +6,10 @@ import { customElement, bindable, bindingMode, inject } from 'aurelia-framework'
 @inject(Element)
 @customElement('aut-microlink')
 export class MicrolinkCustomElement {
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public round: boolean = true;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public url: string = null;
+
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public class: string = '';
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public round: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public url: string = null;
 
   private microlink: HTMLAnchorElement;
   constructor(private element: Element) {
@@ -24,7 +26,6 @@ export class MicrolinkCustomElement {
   private attached() {
     let id = this.generateUniqueId();
     this.microlink.setAttribute('id', id);
-    console.log(this.round);
-    microlink('#' + id, { round: this.round });
+    microlink(`#${id}`, { round: this.round });
   }
 }
