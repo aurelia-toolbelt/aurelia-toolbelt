a7a9360695a99fc0264c1dfbe24880083fc3cb94
Hamed <hamedfathi@outlook.com>
2017-12-24 22:39:39 +0330

    add(password sample)

--- sample/src/routes/bootstrap/password.ts
+++ sample/src/routes/bootstrap/password.ts
@@ -1,3 +1,13 @@
 export class BootstrapPassword {
-
+    public requirements = {
+        minLength: 5
+    };
+    public scoreRange = {
+        '40': { message: 'veryWeak', color: 'red' },
+        '80': { message: 'weak', color: 'khaki' },
+        '120': { message: 'medium', color: 'orange' },
+        '180': { message: 'strong', color: 'maroon' },
+        '200': { message: 'veryStrong', color: 'blue' },
+        '_': { message: 'perfect', color: 'green' }
+    };
 }
