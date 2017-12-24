599d3d20cfcd0881a02b9de0a7148d568146878e
Saeed Ganji <shahabganji@gmail.com>
2017-12-24 22:44:59 +0330

    update(keep track of selected theme;9C)

--- sample/src/components/main-header.ts
+++ sample/src/components/main-header.ts
@@ -4,15 +4,25 @@ import { Router } from 'aurelia-router';
 
 
 class Theme {
-    public name: string;
-    public path: string;
+  public name: string;
+  public path: string;
 }
 
 
 export class MainHeader {
 
-    @bindable({ defaultBindingMode: bindingMode.twoWay }) private  router: Router;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) private router: Router;
 
-    @bindable({ defaultBindingMode: bindingMode.twoWay }) private  themes: Array<Theme>;
-    @bindable({ defaultBindingMode: bindingMode.twoWay }) private theme: Theme;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) private themes: Array<Theme>;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) private theme: Theme;
+
+
+  private bind() {
+    let storedTheme = <Theme>JSON.parse(sessionStorage.getItem('selectedTheme'));
+    if (!storedTheme) {
+      this.theme = this.themes[0];
+    }
+    let index = this.themes.findIndex(x => x.path === storedTheme.path);
+    this.theme = this.themes[index];
+  }
 }
