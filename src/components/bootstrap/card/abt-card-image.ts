c532cc5b1d2159b46fea060bdcc09d8388441fab
Saeed Ganji <shahabganji@gmail.com>
2017-12-25 11:29:29 +0330

    update(abt-card-image) && add(abt-card-* : group & deck)

--- src/components/bootstrap/card/abt-card-image.ts
+++ src/components/bootstrap/card/abt-card-image.ts
@@ -1,15 +1,34 @@
-import { containerless, customElement, bindable, bindingMode } from 'aurelia-framework';
+import { containerless, customElement, bindable, bindingMode, inject } from 'aurelia-framework';
+import { throws } from 'assert';
 
 
+@inject(Element)
 @containerless()
 @customElement('abt-card-image')
 export class BootstrapCardImage {
-
 
     @bindable({ defaultBindingMode: bindingMode.oneTime }) public alt: string;
     @bindable({ defaultBindingMode: bindingMode.oneTime }) public style: string;
     @bindable({ defaultBindingMode: bindingMode.oneTime }) public class: string;
 
     @bindable({ defaultBindingMode: bindingMode.oneWay }) public src: string;
+
+    private cssClass: string = 'card-image';
+
+    constructor(private element: Element) { }
+
+    private attached() {
+
+        let beOnBottom = this.element.hasAttribute('bottom');
+        let beOnTop = this.element.hasAttribute('top');
+
+        if (beOnBottom && beOnTop) {
+            let error = new Error(`The [abt-card-image] should have either 'top' or 'bottom' attributes, and not both of them simultaneously.`);
+            throw error;
+        }
+
+        this.cssClass = beOnBottom ? '-bottom' : beOnTop ? '-top' : '';
+
+    }
 
 }
--- src/components/bootstrap/card/index.ts
+++ src/components/bootstrap/card/index.ts
@@ -4,8 +4,12 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 export function configure(config: FrameworkConfiguration) {
     config.globalResources([
         PLATFORM.moduleName('./abt-card-body.html'),
+        PLATFORM.moduleName('./abt-card-columns.html'),
+        PLATFORM.moduleName('./abt-card-deck.html'),
         PLATFORM.moduleName('./abt-card-footer.html'),
+        PLATFORM.moduleName('./abt-card-group.html'),
         PLATFORM.moduleName('./abt-card-header.html'),
+        PLATFORM.moduleName('./abt-card-image-overlay.html'),
         PLATFORM.moduleName('./abt-card-image'),
         PLATFORM.moduleName('./abt-card-subtitle.html'),
         PLATFORM.moduleName('./abt-card-text'),
