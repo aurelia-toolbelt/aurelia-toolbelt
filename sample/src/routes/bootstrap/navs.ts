5bb9e18686f371f17135583c27ec8f110c013a87
Saeed Ganji <shahabganji@gmail.com>
2017-12-30 14:11:10 +0330

    merge(bootstrap, navs)

--- sample/src/routes/bootstrap/navs.ts
+++ sample/src/routes/bootstrap/navs.ts
@@ -2,4 +2,13 @@
 
 export class BootstrapNavs {
 
+  private testShown(active: any, prev: any) {
+
+    console.log('tab shown event fired');
+    console.warn(active);
+    console.warn(prev);
+
+
+    console.log('###################################################################################');
+  }
 }
