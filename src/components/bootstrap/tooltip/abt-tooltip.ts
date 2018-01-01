a6283a3cb0702b5fe93cb537dc3d3d09530a4983
Hamed Fathi <noeffect@outlook.com>
2018-01-01 15:45:22 +0330

    update

--- src/components/bootstrap/tooltip/abt-tooltip.ts
+++ src/components/bootstrap/tooltip/abt-tooltip.ts
@@ -20,9 +20,14 @@ export class BootstrapTooltipCustomElement {
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public offset: string | number = 0;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public fallbackPlacement: string | string[] = 'flip';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public boundary: BoundaryType = 'scrollParent';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public template: string =
+    '<div class="tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>';
 
-  // Should check
-  // @bindable({ defaultBindingMode: bindingMode.oneWay }) public template: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public show: Function;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public shown: Function;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public hide: Function;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public hidden: Function;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public inserted: Function;
 
   private tooltip: Element;
 
@@ -32,11 +37,15 @@ export class BootstrapTooltipCustomElement {
 
   private attached() {
     let elem = this.element.parentElement;
-    let slotContent = this.tooltip.textContent;
+    let slotContent = this.tooltip.innerHTML;
+
+
+    console.log(this.tooltip.innerHTML);
     // @ts-ignore
     $(elem).tooltip({
       'title': slotContent,
       'html': this.html,
+      'template': this.template,
       'animation': this.animation,
       'placement': this.placement,
       'container': this.container,
@@ -47,5 +56,24 @@ export class BootstrapTooltipCustomElement {
       'boundary': this.boundary
     });
     this.tooltip.remove();
+    if (this.show) {
+      $(elem).on('show.bs.tooltip', this.show());
+    }
+
+    if (this.shown) {
+      $(elem).on('shown.bs.tooltip	', this.shown());
+    }
+
+    if (this.hide) {
+      $(elem).on('hide.bs.tooltip	', this.hide());
+    }
+
+    if (this.hidden) {
+      $(elem).on('hidden.bs.tooltip	', this.hidden());
+    }
+
+    if (this.inserted) {
+      $(elem).on('inserted.bs.tooltip	', this.inserted());
+    }
   }
 }
