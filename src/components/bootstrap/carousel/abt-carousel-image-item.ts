c6f13fff3394e67ca28bef3ba792bfba571ee6fe
Hamed Fathi <noeffect@outlook.com>
2018-01-03 11:20:17 +0330

    init

--- src/components/bootstrap/carousel/abt-carousel-image-item.ts
+++ src/components/bootstrap/carousel/abt-carousel-image-item.ts
@@ -1,8 +1,8 @@
 import { inject, customElement, bindingMode, bindable, containerless } from 'aurelia-framework';
 @inject(Element)
 @containerless()
-@customElement('abt-carousel-item')
-export class CarouselItemCustomElement {
+@customElement('abt-carousel-image-item')
+export class CarouselImageItemCustomElement {
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public src: string;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public alt: string;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public class: string = 'd-block w-100';
