acc3f0a5701622995b715a37c7bce717779c0e0d
Hamed Fathi <noeffect@outlook.com>
2018-01-10 11:15:43 +0330

    BootstrapTypographyService added

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,12 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 export function configure(config: FrameworkConfiguration) {
 
   config
-    // .feature(PLATFORM.moduleName('aurelia-toolbelt/binding-behaviours/index'))
+    .feature(PLATFORM.moduleName('aurelia-toolbelt/binding-behaviours/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/components/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/custom-attributes/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/services/index'))
-    // .feature(PLATFORM.moduleName('aurelia-toolbelt/utilities/index'))
+    .feature(PLATFORM.moduleName('aurelia-toolbelt/utilities/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/value-converters/index'))
     ;
-
-    config.plugin(PLATFORM.moduleName('aurelia-after-attached-plugin'));
+  config.plugin(PLATFORM.moduleName('aurelia-after-attached-plugin'));
 }
