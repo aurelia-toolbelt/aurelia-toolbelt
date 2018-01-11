c1d0c5bb8299f465dcfd19e30d0468a0bb1bbb3b
Hamed <hamedfathi@outlook.com>
2018-01-11 21:39:20 +0330

    rewrite based on <a>

--- src/components/bootstrap/listgroup/abt-listgroup-item.ts
+++ src/components/bootstrap/listgroup/abt-listgroup-item.ts
@@ -1,15 +1,26 @@
-import { inject, customElement, bindingMode, bindable, containerless } from 'aurelia-framework';
+import { inject, customElement, bindingMode, bindable, containerless, DOM } from 'aurelia-framework';
 import * as $ from 'jquery';
 
 @containerless()
 @customElement('abt-listgroup-item')
 export class ListGroupItemCustomElement {
 
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public href: string;
+
     @bindable({ defaultBindingMode: bindingMode.oneWay }) public style: string = '';
     @bindable({ defaultBindingMode: bindingMode.oneWay }) public class: string;
 
     private listgroupitemtmpl: Element;
-    private listgroupitem: Element;
+    private listgroupitem: HTMLLinkElement;
+
+    constructor() {
+        DOM.injectStyles(`
+            a.abt-listgroup-item-disabled {
+                pointer-events: none !important;
+                cursor: default !important;
+            }
+    `);
+    }
 
     private attached() {
         let isActive = this.listgroupitemtmpl.hasAttribute('active');
@@ -19,6 +30,12 @@ export class ListGroupItemCustomElement {
         }
         if (isDisabled) {
             this.listgroupitem.classList.add('disabled');
+        }
+        if (this.href) {
+            $(this.listgroupitem).removeClass('abt-listgroup-item-disabled');
+        } else {
+            $(this.listgroupitem).addClass('abt-listgroup-item-disabled');
+
         }
     }
 }
