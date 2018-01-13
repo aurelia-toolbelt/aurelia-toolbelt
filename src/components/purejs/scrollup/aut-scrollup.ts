bc56ba3117a09b448654f71e46ba319a4c378992
Hamed <hamedfathi@outlook.com>
2018-01-13 22:15:48 +0330

    aut-scrollup added

--- src/components/purejs/scrollup/aut-scrollup.ts
+++ src/components/purejs/scrollup/aut-scrollup.ts
@@ -1,2 +1,23 @@
-export class ScrollUp {
+
+import { customElement, bindable, bindingMode, inject } from 'aurelia-framework';
+import { ZenscrollService } from '../../../services/purejs/zenscroll/zenscroll-service';
+
+// @inject(Element, ZenscrollService)
+@customElement('aut-scrollup')
+export class ScrollUpCustomElement {
+    constructor() {
+        let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    }
+    private myFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            console.log('<50');
+        } else {
+            console.log('>50');
+        }
+    }
+
+    private attached() {
+        window.onscroll = () => this.myFunction();
+    }
 }
