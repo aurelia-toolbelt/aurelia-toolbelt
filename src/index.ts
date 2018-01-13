3d14a1722112ff05cfae077d1ef90a01cac3ddc7
Saeed Ganji <shahabganji@gmail.com>
2018-01-13 14:58:01 +0330

    move(services export to root index.ts )

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 export * from './services/jquery/toastr/toastr-service-options';
 export * from './services/jquery/toastr/toastr-service';
 
+export * from './services/bootstrap/bootstrap-typography-service';
+
+export * from './services/purejs/zenscroll/zenscroll-service-options';
+export * from './services/purejs/zenscroll/zenscroll-service';
 
 export function configure(config: FrameworkConfiguration) {
 
@@ -12,7 +16,6 @@ export function configure(config: FrameworkConfiguration) {
     .feature(PLATFORM.moduleName('aurelia-toolbelt/binding-behaviours/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/components/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/custom-attributes/index'))
-    .feature(PLATFORM.moduleName('aurelia-toolbelt/services/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/utilities/index'))
     .feature(PLATFORM.moduleName('aurelia-toolbelt/value-converters/index'))
     ;
