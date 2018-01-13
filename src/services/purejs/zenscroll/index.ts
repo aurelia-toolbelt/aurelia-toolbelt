b8a6d71475480c19b35e56f79697399ff34c090d
Saeed Ganji <shahabganji@gmail.com>
2018-01-13 09:17:23 +0330

    comment(zenscroll)

--- src/services/purejs/zenscroll/index.ts
+++ src/services/purejs/zenscroll/index.ts
@@ -1,7 +1,7 @@
 import { IZenscrollServiceOptions } from './zenscroll-service-options';
 import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
-export function configure(config: FrameworkConfiguration, options?: IZenscrollServiceOptions) {
-    config.globalResources([PLATFORM.moduleName('./zenscroll-service')]);
-    config.container.registerInstance('zenscroll-service-options', options);
-}
+// export function configure(config: FrameworkConfiguration, options?: IZenscrollServiceOptions) {
+//     config.globalResources([PLATFORM.moduleName('./zenscroll-service')]);
+//     config.container.registerInstance('zenscroll-service-options', options);
+// }
