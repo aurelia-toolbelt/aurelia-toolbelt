64b2ad15f8c0bcd8aa23bff560ac0efe2e501a48
Saeed Ganji <shahabganji@gmail.com>
2018-01-15 21:55:23 +0330

    resolves #30, adhere to standards, array immutability

--- src/value-converters/purejs/array/order-by.ts
+++ src/value-converters/purejs/array/order-by.ts
@@ -2,28 +2,28 @@
 
 export class OrderByValueConverter {
 
-    public toView(array: Array<any>, property: string, direction = 'desc') {
+  public toView(array: Array<any>, property: string, direction = 'desc') {
 
-        array = array.slice(0); //  creates a shallow copy of an array
-        //  value converters should absolutely not modify their source value.
-        //  It is not the expected behavior, so such a converter would be surprising,
-        //  in a really bad way, to developers using it
+    let _array = array.slice(0); //  creates a shallow copy of an array
+    //  value converters should absolutely not modify their source value.
+    //  It is not the expected behavior, so such a converter would be surprising,
+    //  in a really bad way, to developers using it
 
-        const directionFactor = direction === 'desc' ? -1 : 1;
+    const directionFactor = direction === 'desc' ? -1 : 1;
 
-        array.sort((current, next) => {
-            const currentValue = current[property];
-            const nextValue = next[property];
+    _array.sort((current, next) => {
+      const currentValue = current[property];
+      const nextValue = next[property];
 
-            if (currentValue > nextValue) {
-                return directionFactor;
-            } else if (currentValue < nextValue) {
-                return -directionFactor;
-            }
+      if (currentValue > nextValue) {
+        return directionFactor;
+      } else if (currentValue < nextValue) {
+        return -directionFactor;
+      }
 
-            return 0;
-        });
+      return 0;
+    });
 
-        return array;
-    }
+    return _array;
+  }
 }
