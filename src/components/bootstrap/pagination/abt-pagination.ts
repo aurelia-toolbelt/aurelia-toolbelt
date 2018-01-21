8d2400f98e44ad3e6cb21343dbfcdd79ab503a58
Hamed <hamedfathi@outlook.com>
2018-01-21 21:54:59 +0330

    init(abt-pagination)

--- src/components/bootstrap/pagination/abt-pagination.ts
+++ src/components/bootstrap/pagination/abt-pagination.ts
@@ -5,4 +5,29 @@ import { customElement, inject, bindable, bindingMode, BindingEngine, containerl
 @customElement('abt-pagination')
 export class BootstrapPaginationCustomElement {
 
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public totalPages: number = 1;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public startPage: number = 1;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public visiblePages: number = 5;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public hideOnlyOnePage: boolean = true;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public boundaryLinks: boolean = true;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public directionLinks: boolean = true;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public pageVariable: number;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public totalPagesVariable: number;
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public first: string = 'First';
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public last: string = 'Last';
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public prev: string = 'Previous';
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public next: string = 'Next';
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public loop: boolean = false;
+
+    @bindable({ defaultBindingMode: bindingMode.oneWay }) public pageChanged: Function;
+
+
+    private pagination: Element;
+    private paginationItems: Element;
+    private pages: string[] = ['1', '2', '3'];
+
+
+    private afterAttached() {
+        // A
+    }
 }
