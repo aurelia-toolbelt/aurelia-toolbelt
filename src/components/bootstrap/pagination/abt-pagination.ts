94edd536b58e29ce296ab3a67e9a2b7b89bbb257
Hamed Fathi <noeffect@outlook.com>
2018-01-23 11:38:15 +0330

    update(abt-pagination click)

--- src/components/bootstrap/pagination/abt-pagination.ts
+++ src/components/bootstrap/pagination/abt-pagination.ts
@@ -13,8 +13,8 @@ export class BootstrapPaginationCustomElement {
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public directionLinks: boolean = true;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public showGoto: boolean = true;
 
-  // @bindable({ defaultBindingMode: bindingMode.oneWay }) public pageVariable: string;
-  // @bindable({ defaultBindingMode: bindingMode.oneWay }) public totalPagesVariable: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public pageTemplate: string = '%s';
+
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public first: string = 'First';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public last: string = 'Last';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public prev: string = 'Previous';
@@ -30,16 +30,34 @@ export class BootstrapPaginationCustomElement {
 
   private onClick(event: Event) {
     if (this.click) {
+
+
+      let page = (<HTMLAnchorElement>event.target).textContent.trim();
+      let pageNumber = this.getPurePageNumber(page);
+      this.pages = this.createVisibleItems(this.visiblePages, pageNumber, this.totalPages);
+
       this.click({ event: event });
       console.log(event.target);
     }
     return false;
   }
 
+  private getPurePageNumber(template: string): number {
+    let arr = template.split(' ');
+    let arrTemplate = this.pageTemplate.split(' ');
+    for (let index = 0; index < arrTemplate.length; index++) {
+      let value = arrTemplate[index];
+      if (value === '%s') {
+        return parseInt(arr[index].trim(), 10);
+      }
+    }
+    throw Error('Check your page template maybe have some problem.');
+  }
+
   private createVisibleItems(visibleItem: number, selectedItem: number, totalPages: number): string[] {
     let items: string[] = [];
     for (let index = 0; index < visibleItem; index++) {
-      items[index] = 'a';
+      items[index] = '-1';
     }
     let leftSide = Math.ceil(visibleItem / 2);
     let rightSide = Math.floor(visibleItem / 2);
@@ -72,7 +90,7 @@ export class BootstrapPaginationCustomElement {
       if (items[index] === selectedItem.toString()) {
         isBefore = false;
       }
-      if (items[index] === 'a' && !isBefore) {
+      if (items[index] === '-1' && !isBefore) {
         currentItem += 1;
         items[index] = currentItem.toString();
       }
@@ -84,7 +102,7 @@ export class BootstrapPaginationCustomElement {
       if (items[index] === selectedItem.toString()) {
         isAfter = false;
       }
-      if (items[index] === 'a' && !isAfter) {
+      if (items[index] === '-1' && !isAfter) {
         currentItemReverse -= 1;
         items[index] = currentItemReverse.toString();
       }
@@ -103,11 +121,16 @@ export class BootstrapPaginationCustomElement {
   private afterAttached() {
 
     if (this.visiblePages <= 0) {
-      throw Error('you are a stupid vicious programmer');
+      throw Error('The visible pages value should be greater than 0.');
     }
 
     if (this.visiblePages > this.totalPages) {
-      throw Error('you are a stupid vicious programmer');
+      throw Error('The visible pages should always be less than or equal to the total pages.');
+    }
+
+    if (this.hideOnlyOnePage) {
+      this.boundaryLinks = false;
+      this.directionLinks = false;
     }
 
 
