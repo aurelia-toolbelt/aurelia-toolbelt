123737c825d7f19276ba5b2bad005720a68c2a99
Hamed Fathi <noeffect@outlook.com>
2018-01-24 15:10:54 +0330

    init(carousel doc)

--- src/components/bootstrap/carousel/abt-carousel-image-item.ts
+++ src/components/bootstrap/carousel/abt-carousel-image-item.ts
@@ -9,6 +9,7 @@ export class CarouselImageItemCustomElement {
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public src: string;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public alt: string;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public class: string = 'd-block w-100';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public active: boolean | string;
 
   private isActive: boolean = false;
   private carouselItem: Element;
@@ -18,11 +19,11 @@ export class CarouselImageItemCustomElement {
 
   }
 
-  private createIndicatorHtml(id: string, index: number, beActive: boolean): any {
-    return `<li style="cursor:pointer" data-target="#${id}" data-slide-to="${index}" class="${beActive ? 'active' : ''}" ></li>`;
+  private createIndicatorHtml(id: string, index: number): any {
+    return `<li style="cursor:pointer" data-target="#${id}" data-slide-to="${index}" class="${this.active ? 'active' : ''}" ></li>`;
   }
 
-  private attached() {
+  private afterAttached() {
 
     this.times = this.sharedController.getAndIncrement();
     let isActive = this.element.hasAttribute('active');
@@ -30,9 +31,9 @@ export class CarouselImageItemCustomElement {
     let carouselOl = carousel.children[0];
     let isOl = carouselOl.nodeName.toLowerCase() === 'ol';
     if (isOl) {
-      carouselOl.innerHTML += this.createIndicatorHtml(carousel.id, this.times, isActive);
+      carouselOl.innerHTML += this.createIndicatorHtml(carousel.id, this.times);
     }
-    if (isActive) {
+    if (this.active) {
       this.carouselItem.classList.add('active');
     }
   }
--- src/components/bootstrap/carousel/abt-carousel.ts
+++ src/components/bootstrap/carousel/abt-carousel.ts
@@ -4,26 +4,34 @@ import * as $ from 'jquery';
 @containerless()
 @customElement('abt-carousel')
 export class CarouselCustomElement {
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public prevControl: boolean = false;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public nextControl: boolean = false;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public prevControlTitle: string = 'Previous';
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public nextControlTitle: string = 'Next';
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public prevControlClass: string = 'carousel-control-prev-icon';
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public nextControlClass: string = 'carousel-control-next-icon';
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public showIndicator: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public prevTitle: string = 'Previous';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public nextTitle: string = 'Next';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public prevClass: string = 'carousel-control-prev-icon';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public nextClass: string = 'carousel-control-next-icon';
 
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public interval: number = 5000;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public keyboard: boolean = true;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public pause: boolean = true;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public ride: boolean = false;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public wrap: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public showNavigator: boolean | string = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public showIndicator: boolean | string = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public interval: number | string = 5000;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public keyboard: boolean | string = true;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public pause: boolean | string = true;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public ride: boolean | string = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public wrap: boolean | string = true;
 
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public slide: Function;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public slid: Function;
 
 
   private carousel: Element;
-  private attached() {
+  private afterAttached() {
+
+    this.showNavigator = Boolean(this.showNavigator);
+    this.showIndicator = Boolean(this.showIndicator);
+    this.interval = Number(this.interval);
+    this.keyboard = Boolean(this.keyboard);
+    this.pause = Boolean(this.pause);
+    this.ride = Boolean(this.ride);
+    this.wrap = Boolean(this.wrap);
+
     // @ts-ignore
     $(this.carousel).carousel({
       interval: this.interval,
