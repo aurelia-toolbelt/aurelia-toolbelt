ae06e8064d073955ad39d4f0e141f6ff41ddaa39
Saeed Ganji <shahabganji@gmail.com>
2018-01-27 01:09:20 +0330

    update(all parts of abt-alert)

--- src/components/bootstrap/alert/abt-alert-link.ts
+++ src/components/bootstrap/alert/abt-alert-link.ts
@@ -7,12 +7,7 @@ import { inject, containerless, customElement, bindable, bindingMode } from 'aur
 @containerless()
 @customElement('abt-alert-link')
 export class BootstrapAlertLink {
-
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public id: string = '';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public href: string = '';
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public routeHref: any;
-
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) public disabled: boolean | string;
-
-  constructor(private element: Element) { }
-
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public disabled: boolean | string = false;
 }
