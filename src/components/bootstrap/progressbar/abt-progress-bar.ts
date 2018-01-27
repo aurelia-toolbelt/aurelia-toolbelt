f6c0559286b8646961431c0c8d24a238e3c96cd2
Hamed <hamedfathi@outlook.com>
2018-01-28 00:28:15 +0330

    complete(abt-progress doc)

--- src/components/bootstrap/progressbar/abt-progress-bar.ts
+++ src/components/bootstrap/progressbar/abt-progress-bar.ts
@@ -1,21 +1,22 @@
 import { Uuid } from './../../../utilities/purejs/uuid';
 import { inject, customAttribute, bindingMode, bindable, customElement, DOM, containerless } from 'aurelia-framework';
-
+export type ColorType = 'primary' | 'secondary' | 'success' | 'danger'
+  | 'warning' | 'info' | 'light' | 'dark';
 @inject(Uuid)
 @customElement('abt-progress-bar')
 @containerless()
 export class BootstrapProgressBar {
 
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public colorType: string;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public color: string;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public gradientColor: string;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public style: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public colorType: ColorType;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public color: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public gradientColor: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public style: string;
   @bindable({ defaultBindingMode: bindingMode.oneTime }) public class: string;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public value: string;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public min: string;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public max: string;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public animated: boolean | string = false;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public striped: boolean | string = false;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public value: number | string = 0;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public min: number | string = 0;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public max: number | string = 100;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public animated: boolean | string = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public striped: boolean | string = false;
 
   private isAnimated: boolean = false;
   private isStriped: boolean = false;
@@ -27,10 +28,11 @@ export class BootstrapProgressBar {
   }
 
   private afterAttached() {
-
-
     this.animated = Boolean(this.animated) || this.progressbarTemplate.hasAttribute('animated');
     this.striped = Boolean(this.striped) || this.progressbarTemplate.hasAttribute('striped');
+    this.value = Number(this.value);
+    this.min = Number(this.min);
+    this.max = Number(this.max);
 
     this.id = this.uuid.Uuidv4ForId();
 
--- src/components/bootstrap/progressbar/abt-progress.ts
+++ src/components/bootstrap/progressbar/abt-progress.ts
@@ -1,6 +1,5 @@
 import { inject, customAttribute, bindingMode, bindable, customElement, DOM, containerless } from 'aurelia-framework';
 
-@containerless()
 @customElement('abt-progress')
 export class BootstrapProgress {
   @bindable({ defaultBindingMode: bindingMode.oneTime }) public id: string;
