071cb4f1efb972a69a06cd8875197ba21f6090e8
Saeed Ganji <shahabganji@gmail.com>
2018-01-28 13:39:09 +0330

    docs( abt-link-button )

---
+++ sample/docs/components/bootstrap/button-group/usage.md
@@ -0,0 +1,12 @@
+
+you should registered the plugin beforehand
+
+```js
+aurelia.plugin(PLATFORM.moduleName('aurelia-toolbelt'));
+```
+or at any further level you are interested down to the ```button``` itself
+```js
+aurelia.plugin(PLATFORM.moduleName('aurelia-toolbelt/components/bootstrap/button'));
+```
+
+**PS:** Button groups do exists in button path.
--- src/components/bootstrap/button/abt-link-button.ts
+++ src/components/bootstrap/button/abt-link-button.ts
@@ -6,23 +6,68 @@ import { customElement, inject, containerless, bindable, bindingMode } from 'aur
 @customElement('abt-link-button')
 export class BootstrapLinkButton {
 
-    @bindable({ defaultBindingMode: bindingMode.oneTime }) public color: string = 'primary';
-    @bindable({ defaultBindingMode: bindingMode.oneTime }) public size: string = 'md';
-    @bindable({ defaultBindingMode: bindingMode.oneTime }) public type: string = 'button';
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public type: string = 'primary';
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public size: string = 'md';
 
-    @bindable({ defaultBindingMode: bindingMode.oneWay }) public href: string = '';
-    @bindable({ defaultBindingMode: bindingMode.oneWay }) public routeHref: any;
-    @bindable({ defaultBindingMode: bindingMode.oneTime }) public id: string;
-    @bindable({ defaultBindingMode: bindingMode.twoWay }) public disabled: boolean | string;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public outline: boolean | string = false;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public block: boolean | string = false;
 
-    private isOutlined: boolean = false;
-    private isBlockLevel: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public style: string = '';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public class: string = '';
 
-    constructor(private element: Element) { }
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public href: string = '';
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public id: string;
 
-    private bind() {
-        this.isOutlined = this.element.hasAttribute('outline');
-        this.isBlockLevel = this.element.hasAttribute('block');
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public click: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public disabled: boolean | string;
+
+  private isBusy: boolean = false;
+  private task: Promise<void> | null = null;
+
+  constructor(private element: Element) { }
+
+  private afterAttached() {
+
+    const onlyOutlineAttribute = (this.outline === '' && this.element.hasAttribute('outline'));
+    this.outline = onlyOutlineAttribute || this.outline === 'true' || this.outline === true;
+
+    const onlyBlockAttribute = (this.block === '' && this.element.hasAttribute('block'));
+    this.block = onlyBlockAttribute || this.block === 'true' || this.block === true;
+
+    const onlyDisabledAttribute = (this.disabled === '' && this.element.hasAttribute('disabled'));
+    this.disabled = onlyDisabledAttribute || this.disabled === 'true' || this.disabled === true;
+
+  }
+
+  private onClick(event: Event) {
+
+    if (!this.click) {
+      return true;
     }
 
+    event.preventDefault();
+
+    if (this.task || this.disabled) {
+      return false;
+    }
+
+    this.isBusy = true;
+
+    this.task = Promise.resolve(this.click({ event: event, target: event.target }))
+      .then(
+      () => this.clickCompleted(),
+      () => this.clickCompleted()
+      );
+
+    return false;
+  }
+
+  private clickCompleted() {
+    this.task = null;
+    this.isBusy = false;
+  }
+
+  private detached() {
+    this.task = null;
+  }
 }
