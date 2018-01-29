708ae7cf0829fc147c85ee971de5d8f2dffdfe38
Hamed <hamedfathi@outlook.com>
2018-01-29 19:28:46 +0330

    complete(abt-popover docs)

--- src/components/bootstrap/popover/abt-popover.ts
+++ src/components/bootstrap/popover/abt-popover.ts
@@ -9,32 +9,39 @@ import * as $ from 'jquery';
 @customElement('abt-popover')
 export class BootstrapPopoverCustomElement {
 
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public animation: boolean = true;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public container: string | boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public animation: boolean | string = true;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public container: boolean | string | Element = false;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public delay: number | object = 0;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public html: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public html: boolean | string = false;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public placement: Placement | Function = 'right';
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public selector: string | boolean = false;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public title: string | Function = '';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public selector: boolean | string = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public title: string | Element | Function = '';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public trigger: string = 'click';
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public offset: string | number = 0;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public offset:  number | string = 0;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public fallbackPlacement: string | string[] = 'flip';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public boundary: Boundary = 'scrollParent';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public template: string =
     '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>';
 
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public showPopover: Function;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public popoverShown: Function;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public hidePopover: Function;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public popoverHidden: Function;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public popoverInserted: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public bsShow: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public bsShown: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public bsHide: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public bsHidden: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public bsInserted: Function;
 
   private popover: Element;
+  private popoverTemplate: Element;
   private parentElement: HTMLElement;
 
   private attached() {
     this.parentElement = this.popover.parentElement;
     let slotContent = this.html ? this.popover.innerHTML : this.popover.textContent;
+
+    this.animation = (this.animation === '' && this.popoverTemplate.hasAttribute('animation')) || this.animation.toString() === 'true';
+    this.container = (this.container === '' && this.popoverTemplate.hasAttribute('container')) || this.container.toString() === 'true';
+    this.html = (this.html === '' && this.popoverTemplate.hasAttribute('html')) || this.html.toString() === 'true';
+    this.selector = (this.selector === '' && this.popoverTemplate.hasAttribute('selector')) || this.selector.toString() === 'true';
+
     // @ts-ignore
     $(this.parentElement).popover({
       'content': slotContent,
@@ -50,30 +57,51 @@ export class BootstrapPopoverCustomElement {
       'fallbackPlacement': this.fallbackPlacement,
       'boundary': this.boundary
     });
+
     this.popover.remove();
-    if (this.showPopover) {
+
+    if (this.bsShow) {
       // @ts-ignore
-      $(this.parentElement).on('show.bs.popover', this.showPopover);
+      $(this.parentElement).on('show.bs.popover', () => {
+        if (this.bsShow) {
+          this.bsShow();
+        }
+      });
     }
 
-    if (this.popoverShown) {
+    if (this.bsShown) {
       // @ts-ignore
-      $(this.parentElement).on('shown.bs.popover', this.popoverShown);
+      $(this.parentElement).on('shown.bs.popover', () => {
+        if (this.bsShown) {
+          this.bsShown();
+        }
+      });
     }
 
-    if (this.hidePopover) {
+    if (this.bsHide) {
       // @ts-ignore
-      $(this.parentElement).on('hide.bs.popover', this.hidePopover);
+      $(this.parentElement).on('hide.bs.popover', () => {
+        if (this.bsHide) {
+          this.bsHide();
+        }
+      });
+    }
+    if (this.bsHidden) {
+      // @ts-ignore
+      $(this.parentElement).on('hidden.bs.popover', () => {
+        if (this.bsHidden) {
+          this.bsHidden();
+        }
+      });
     }
 
-    if (this.popoverHidden) {
+    if (this.bsInserted) {
       // @ts-ignore
-      $(this.parentElement).on('hidden.bs.popover', this.popoverHidden);
-    }
-
-    if (this.popoverInserted) {
-      // @ts-ignore
-      $(this.parentElement).on('inserted.bs.popover', this.popoverInserted);
+      $(this.parentElement).on('inserted.bs.popover', () => {
+        if (this.bsInserted) {
+          this.bsInserted();
+        }
+      });
     }
   }
 
