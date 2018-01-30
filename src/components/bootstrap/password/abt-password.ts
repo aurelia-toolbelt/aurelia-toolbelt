d8d627f192b54b971bab3af1ea5108b90f7cbd0b
Hamed <hamedfathi@outlook.com>
2018-01-30 23:21:31 +0330

    overhaul(abt-password)

--- src/components/bootstrap/password/abt-password.ts
+++ src/components/bootstrap/password/abt-password.ts
@@ -5,136 +5,144 @@ import {
   Disposable, BindingEngine
 } from 'aurelia-framework';
 
-@inject(Element, PasswordMeter)
 @customElement('abt-password')
 export class PasswordCustomElement {
-  private showStrength: boolean = false;
+
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public inputClass: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public inputStyle: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public buttonClass: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public buttonStyle: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public errorIcon: string = 'fa fa-times';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public showIcon: string = 'fa fa-eye';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public hideIcon: string = 'fa fa-eye-slash';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public progressBarHeight: string = '5px';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public showTooltip: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public showProgressBar: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public size: string = 'md';
 
   @bindable({ defaultBindingMode: bindingMode.twoWay }) public text: string;
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) public scoreRange: any;
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) public requirements: any;
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) public score: number;
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) public showPassword: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public scoreRange: object = null;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public requirements: object = null;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public passwordVisibility: boolean = true;
 
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public passwordChanged: Function;
 
-  private color: string;
-  private passwordTitle: string;
-  private groupClass: string;
-  private passwordStyle: string;
-  private passwordSpan: string;
-  private passwordClass: string;
-  private textStyle: string;
   private txtPassword: HTMLInputElement;
-  private icon: Element;
-  private invisible: boolean = true;
+  private btnPassword: HTMLButtonElement;
+  private iconPassword: HTMLElement;
 
-  constructor(private element: Element, private passwordMeter: PasswordMeter) {
-    this.element = <HTMLInputElement>element;
+  private isInvisible: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) private progressBarValue = 0;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) private progressBarClass: string;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) private progressBarColor: string;
+
+
+  private passwordMeter: PasswordMeter;
+
+  private afterAttached() {
+    let req = this.requirements;
+    let range = this.getScoreRangeInfo(this.scoreRange);
+    this.passwordMeter = new PasswordMeter(req, range);
   }
 
-  private showPasswordChanged(value: boolean) {
-    if (value) {
-      this.groupClass = 'input-group';
-    } else {
-      this.groupClass = '';
-    }
-  }
-
-  private passwordVisibility() {
-    if (this.invisible) {
-      this.invisible = false;
+  private passwordVisibilityToggle() {
+    if (this.isInvisible) {
+      this.isInvisible = false;
       $(this.txtPassword).attr('type', 'text');
-      this.icon.classList.remove('fa-eye-slash');
-      this.icon.classList.add('fa-eye');
+      this.iconPassword.classList.remove('fa-eye-slash');
+      this.iconPassword.classList.add('fa-eye');
     } else {
-      this.invisible = true;
+      this.isInvisible = true;
       $(this.txtPassword).attr('type', 'password');
-      this.icon.classList.remove('fa-eye');
-      this.icon.classList.add('fa-eye-slash');
+      this.iconPassword.classList.remove('fa-eye');
+      this.iconPassword.classList.add('fa-eye-slash');
     }
   }
-  private findOption(message: string, option: any): any {
-    for (let index = 0; index < option.length; index++) {
-      if (message === option[index].message) {
-        return option[index];
-      }
 
-    }
-  }
   private generateErrorsAsHtml(errors: any): string {
     let html = '';
-
     if (errors) {
       for (let index = 0; index < errors.length; index++) {
         const element = errors[index];
-        html += `<div><i class="fa fa-times" style="color:red;margin-right:5px" aria-hidden="true"></i>${element}</div>`;
+        html += `<div><i class="abt-password-error ${this.errorIcon}" aria-hidden="true"></i>${element}</div>`;
       }
     }
     return html;
   }
-  private textChanged(value: string) {
-    let cls = 'input-group-prepend visible-md-* visible-lg-* ';
-    this.passwordClass = cls;
 
-    let option: any = [];
-    let scoreSetting: any = {};
-    if (this.scoreRange) {
-      let scores = Object.keys(this.scoreRange);
-      for (let index = 0; index < scores.length; index++) {
-        let key = scores[index];
-        let scoreOption = this.scoreRange[scores[index]];
-        option.push(scoreOption);
-        scoreSetting[key] = scoreOption.message;
-      }
+  private getScoreRangeInfo(scoreRange: any): any {
+    let option: any = {};
+    let color = '';
+    let isClass: boolean = false;
+    let scores = Object.keys(scoreRange);
+    for (let index = 0; index < scores.length; index++) {
+      let key = scores[index];
+      let message = scoreRange[key].message;
+      option[key] = message;
     }
-
-    if (value.length > 0) {
-      this.groupClass = 'input-group';
-      this.passwordMeter.requirements = this.requirements;
-      this.passwordMeter.scoreRange = scoreSetting;
-      let obj = this.passwordMeter.getResult(value);
-      let setting: any = this.findOption(obj.status, option);
-
-      if (obj.score < 0) {
-        $(this.txtPassword).tooltip({
-          'title': this.generateErrorsAsHtml(obj.errors),
-          'html': true,
-          'animation': false,
-          'placement': 'bottom'
-        });
-      } else {
-        $(this.txtPassword).tooltip('dispose');
-      }
-
-      if (setting && setting.color !== undefined) {
-        this.color = setting.color;
-      } else {
-        this.color = option[0].color;
-      }
-
-      this.passwordTitle = obj.status;
-
-      let classNames: any = this.findOption(obj.status, option);
-      if (classNames && classNames.class !== undefined) {
-        cls = cls + classNames.class;
-        this.passwordClass = cls;
-        this.passwordStyle = '';
-      } else {
-        this.passwordStyle = 'padding:0.4rem;color:white;background-color:' + this.color + ';border-bottom:3px solid '
-          + this.color + ';border-right:2px solid ' + this.color + ';';
-        this.textStyle = 'border-bottom:3px solid ' + this.color;
-      }
-
-    } else {
-      this.passwordStyle = '';
-      this.textStyle = '';
-      if (this.showPassword) {
-        this.groupClass = 'input-group';
-      } else {
-        this.groupClass = '';
-      }
-    }
-    return true;
+    return option;
   }
 
+  private getMinColorInfo(scoreRange: any): any {
+    let scores = Object.keys(scoreRange);
+    let color = null;
+    let isClass = false;
+    let key = scores[0];
+    let message = scoreRange[key].message;
+    color = scoreRange[key].color;
+    isClass = color.startsWith('.');
+    return {
+      color: color,
+      isClass: isClass
+    };
+  }
+
+  private getColorInfo(scoreRange: any, status: string): any {
+    let scores = Object.keys(scoreRange);
+    let color = null;
+    let isClass = false;
+    for (let index = 0; index < scores.length; index++) {
+      let key = scores[index];
+      let message = scoreRange[key].message;
+      if (message === status) {
+        color = scoreRange[key].color;
+        isClass = color.startsWith('.');
+        break;
+      }
+    }
+    if (!color) {
+      return null;
+    }
+    return {
+      color: color,
+      isClass: isClass
+    };
+  }
+
+  private textChanged(value: string) {
+
+    let result = this.passwordMeter.getResult(value);
+    let colorStatus = this.getColorInfo(this.scoreRange, result.status);
+
+    if (result.score >= 0) {
+      this.progressBarValue = result.percent;
+    } else {
+      this.progressBarValue = 100;
+      colorStatus = this.getMinColorInfo(this.scoreRange);
+    }
+
+    if (colorStatus) {
+      if (colorStatus.isClass) {
+        this.progressBarClass = colorStatus.color;
+      } else {
+        this.progressBarColor = colorStatus.color;
+      }
+    }
+
+    if (this.passwordChanged) {
+      this.passwordChanged({
+        result: result
+      });
+    }
+  }
 }
