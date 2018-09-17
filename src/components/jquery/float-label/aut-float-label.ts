a3f51b784d4bfe343f9291b53a0ada8c3813eb07
Hamed Fathi <noeffect@outlook.com>
2018-09-17 12:00:20 +0430

    update(aut-float-label) update(aut-scrollup z-index)

--- src/components/jquery/float-label/aut-float-label.ts
+++ src/components/jquery/float-label/aut-float-label.ts
@@ -9,45 +9,44 @@ export class BootstrapFloatLabel {
   @bindable({ defaultBindingMode: bindingMode.oneTime }) public id: string;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public style: string;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public class: string;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public labelClass: string;
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public labelStyle: string;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public placeholder: string;
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public labelClass: string = 'primary';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public placeholder: string = '';
+  @bindable({ defaultBindingMode: bindingMode.oneWay }) public placeholderColor: string = 'black';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public type: string = 'text';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public direction: FloatInputDirection = 'ltr';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public size: FloatInputSize = 'md';
   private sizeClass: string = '';
   private labelDirection: string = '';
-
   private floatInput: HTMLInputElement;
+  private floatLabel: HTMLLabelElement;
   private floatLabelTemplate: Element;
   private attached() {
-    let ltr = `.aut-float-label>.float-label { left: 10px; !important }`;
-    let rtl = `.aut-float-label>.float-label { right: 10px; !important }`;
 
     this.sizeClass = this.size === 'sm' ? 'form-control-sm' : (this.size === 'lg' ? 'form-control-lg' : '');
-this.labelDirection = this.direction === 'ltr' ? 'aut-float-label-ltr':'aut-float-label-rtl';
+    this.labelDirection = this.direction === 'ltr' ? 'aut-float-label-ltr' : 'aut-float-label-rtl';
     if ($('.aut-float-label input').length) {
       let aut_float_on_class = 'on';
       let aut_float_show_class = 'show';
 
-      $('.float-input').on('aut-check-value', function() {
-          let _aut_label = $(this).closest('.aut-float-label').find('.float-label');
-          if ((<HTMLInputElement>this).value !== '') {
-              _aut_label.addClass(aut_float_show_class);
-          } else {
-              _aut_label.removeClass(aut_float_show_class);
-          }
+      $('.float-input').on('aut-check-value', function () {
+        let _aut_label = $(this).closest('.aut-float-label').find('.float-label');
+        if ((<HTMLInputElement>this).value !== '') {
+          _aut_label.addClass(aut_float_show_class);
+        } else {
+          _aut_label.removeClass(aut_float_show_class);
+        }
       })
-      .on('keyup', function() {
+        .on('keyup', function () {
           $(this).trigger('aut-check-value');
-      })
-      .on('focus', function() {
+        })
+        .on('focus', function () {
           $(this).closest('.aut-float-label').find('.float-label').addClass(aut_float_on_class);
-      })
-      .on('blur', function() {
+        })
+        .on('blur', function () {
           $(this).closest('.aut-float-label').find('.float-label').removeClass(aut_float_on_class);
-      }).trigger('aut-check-value');
-  }
+        }).trigger('aut-check-value');
+    }
   }
 }
 
