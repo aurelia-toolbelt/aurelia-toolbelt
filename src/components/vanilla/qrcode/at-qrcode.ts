ff3384d7182d6d1ee6b75c8598c6efec45cb02d2
Saeed Ganji <shahabganji@gmail.com>
2018-09-20 19:46:06 +0430

    support unicode barcodes

--- src/components/vanilla/qrcode/at-qrcode.ts
+++ src/components/vanilla/qrcode/at-qrcode.ts
@@ -1,11 +1,21 @@
 import { customElement, bindingMode, bindable, inject } from 'aurelia-framework';
 import { QR8BitByte, QRAlphaNum, QRCode, QRKanji, QRNumber, ErrorCorrectLevel } from 'qrcode-generator-ts';
 
+import {createStringToBytes} from 'qrcode-generator-ts/dist/js/text/createStringToBytes';
+
+export enum QrMode {
+  Numeric = 'Numeric', AlphaNumeric = 'Alphanumeric', Byte = 'Byte', Kanji = 'Kanji'
+}
+
 @customElement('at-qrcode')
 export class AureliaToolbeltQrCode {
 
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public value: string;
 
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public typeNumber: number = 5;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public errorCorrectionLevel: ErrorCorrectLevel = ErrorCorrectLevel.H;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public mode: QrMode;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public isUnicode: boolean = false;
 
   @bindable({ defaultBindingMode: bindingMode.oneTime }) public size: number = 128;
   @bindable({ defaultBindingMode: bindingMode.oneTime }) public darkColor: string = '#000000';
@@ -15,9 +25,18 @@ export class AureliaToolbeltQrCode {
 
   private attached() {
     this.size = Number(this.size);
+
+    this.typeNumber = Number(this.typeNumber);
+
+    if (!(1 <= this.typeNumber && this.typeNumber <= 40)) {
+      Error('TypeNumber should be between 1 and 40');
+    }
+
+    this.isUnicode = Boolean(this.isUnicode);
+
   }
 
-  private createCanvas(qr: QRCode) { // }, cellSize = 2, margin = cellSize * 4) {
+  private drawBarCodeOnCanvas(qr: QRCode) { // }, cellSize = 2, margin = cellSize * 4) {
 
     // let canvas = document.createElement('canvas');
     // let size = qr.getModuleCount() * cellSize + margin * 2;
@@ -53,20 +72,77 @@ export class AureliaToolbeltQrCode {
     if (newValue) {
 
       // uncomment if UTF-8 support is required.
-      // QRCode.stringToBytes = com.d_project.text.stringToBytes_UTF8;
+      QRCode.stringToBytes =  AureliaToolbeltQrCode.stringToBytes_UTF8; // com.d_project.text.stringToBytes_UTF8;
 
       let qr = new QRCode();
-      qr.setTypeNumber(5);
-      qr.setErrorCorrectLevel(ErrorCorrectLevel.L);
-      // qr.addData(new QRNumber('0123')); // Number only
-      // qr.addData(new QRAlphaNum('AB5678CD')); // Alphabet and Number
-      qr.addData(new QR8BitByte(newValue)); // most useful for usual purpose.
-      // qr.addData('[here is 8BitByte too]');
-      // qr.addData(new QRKanji('漢字')); // Kanji(SJIS) only
+
+      qr.setTypeNumber(this.typeNumber);
+      qr.setErrorCorrectLevel(this.errorCorrectionLevel);
+
+      switch (this.mode) {
+        case QrMode.Kanji:
+          qr.addData(new QRKanji(newValue)); // Kanji(SJIS) only
+          break;
+        case QrMode.Numeric:
+          qr.addData(new QRNumber(newValue)); // Number only
+          break;
+        case QrMode.AlphaNumeric:
+          qr.addData(new QRAlphaNum(newValue)); // Alphabet and Number
+          break;
+        default:
+          qr.addData(new QR8BitByte(newValue)); // most useful for usual purpose.
+          break;
+      }
+
+      //  geneartes the data
       qr.make();
 
       // canvas
-      this.createCanvas(qr); // , 2);
+      this.drawBarCodeOnCanvas(qr); // , 2);
     }
   }
+
+  private static stringToBytes_UTF8(s: string): number[] {
+    // http://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
+    function toUTF8Array(str: string): number[] {
+      let utf8: number[] = [];
+      for (let i = 0; i < str.length; i++) {
+        let charcode = str.charCodeAt(i);
+        if (charcode < 0x80) { utf8.push(charcode); } else if (charcode < 0x800) {
+          // tslint:disable-next-line:no-bitwise
+          utf8.push(0xc0 | (charcode >> 6),
+            // tslint:disable-next-line:no-bitwise
+            0x80 | (charcode & 0x3f));
+        } else if (charcode < 0xd800 || charcode >= 0xe000) {
+          // tslint:disable-next-line:no-bitwise
+          utf8.push(0xe0 | (charcode >> 12),
+            // tslint:disable-next-line:no-bitwise
+            0x80 | ((charcode >> 6) & 0x3f),
+            // tslint:disable-next-line:no-bitwise
+            0x80 | (charcode & 0x3f));
+        } else {
+          i++;
+          // UTF-16 encodes 0x10000-0x10FFFF by
+          // subtracting 0x10000 and splitting the
+          // 20 bits of 0x0-0xFFFFF into two halves
+          // tslint:disable-next-line:no-bitwise
+          charcode = 0x10000 + (((charcode & 0x3ff) << 10)
+            // tslint:disable-next-line:no-bitwise
+            | (str.charCodeAt(i) & 0x3ff));
+          // tslint:disable-next-line:no-bitwise
+          utf8.push(0xf0 | (charcode >> 18),
+            // tslint:disable-next-line:no-bitwise
+            0x80 | ((charcode >> 12) & 0x3f),
+            // tslint:disable-next-line:no-bitwise
+            0x80 | ((charcode >> 6) & 0x3f),
+            // tslint:disable-next-line:no-bitwise
+            0x80 | (charcode & 0x3f));
+        }
+      }
+      return utf8;
+    }
+    return toUTF8Array(s);
+  }
+
+
 }
