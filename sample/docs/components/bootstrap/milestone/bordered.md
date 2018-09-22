a1e2e7215eb9be2639291a96a80d3a175da55d92
Saeed Ganji <shahabganji@gmail.com>
2018-09-22 17:51:51 +0330

    docs( init at-milestone )

---
+++ sample/docs/components/bootstrap/milestone/bordered.md
@@ -0,0 +1,25 @@
+```css
+<style>
+  .milestone-aurelia {
+      background-color: #753B85;
+      color: white;
+      border: 1px solid darkgoldenrod;
+  }
+</style>
+```
+```html
+<at-milestone-container top-border bottom-border>
+  <at-milestone type="aurelia" icon="fa fa-paperclip">
+    ...
+  </at-milestone>
+  <at-milestone icon="fa fa-comment">
+    ...
+  </at-milestone>
+  <at-milestone type="success" icon="fa fa-check">
+    ...
+  </at-milestone>
+  <at-milestone type="warning" icon="fa fa-eye">
+    ...
+  </at-milestone>
+</at-milestone-container>
+```
--- src/components/bootstrap/milestone/at-milestone-container.ts
+++ src/components/bootstrap/milestone/at-milestone-container.ts
@@ -1,24 +1,27 @@
-import { useShadowDOM, customElement, containerless, bindable, bindingMode } from 'aurelia-framework';
+import { useShadowDOM, customElement, containerless, bindable, bindingMode, inject } from 'aurelia-framework';
 
 
+@inject(Element)
 @containerless()
 @customElement('at-milestone-container')
 export class AureliaToolbeltMilestoneContainer {
 
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public topBorder: boolean = true;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public bottomBorder: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public topBorder: boolean | string = true;
+  @bindable({ defaultBindingMode: bindingMode.oneTime }) public bottomBorder: boolean | string = true;
 
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public style: string = '';
   @bindable({ defaultBindingMode: bindingMode.oneWay }) public class: string = '';
 
+  constructor(private element: Element) { }
+
 
   public attached() {
 
-    const onlyTopBorderAttribute = (this.topBorder == '' && this.element.hasAttribute('top-border'));
-    this.topBorder = onlyTopBorderAttribute || this.topBorder == 'true' || this.topBorder == true;
+    const onlyTopBorderAttribute = (this.topBorder === '' && this.element.hasAttribute('top-border'));
+    this.topBorder = onlyTopBorderAttribute || this.topBorder === 'true' || this.topBorder === true;
 
-    const onlyBottomBorderAttribute = (this.bottomBorder == '' && this.element.hasAttribute('bottom-border'));
-    this.bottomBorder = onlyBottomBorderAttribute || this.bottomBorder == 'true' || this.bottomBorder == true;
+    const onlyBottomBorderAttribute = (this.bottomBorder === '' && this.element.hasAttribute('bottom-border'));
+    this.bottomBorder = onlyBottomBorderAttribute || this.bottomBorder === 'true' || this.bottomBorder === true;
 
   }
 
