83eafcedeca1b8209f78765a359ae7c7c1ba34a2
Hamed Fathi <noeffect@outlook.com>
2018-11-10 12:26:37 +0330

    feat(at-max-length)

--- src/custom-attributes/jquery/max-length/at-max-length.ts
+++ src/custom-attributes/jquery/max-length/at-max-length.ts
@@ -6,6 +6,52 @@ import {
   customAttribute
 } from 'aurelia-framework';
 
+export type MaxLengthPlacement =
+  | 'bottom'
+  | 'left'
+  | 'top'
+  | 'right'
+  | 'bottom-right'
+  | 'top-right'
+  | 'top-left'
+  | 'bottom-left'
+  | 'centered-right';
+
+interface PlacementOptions {
+  /**
+   * The top position of the counter (Number of pixels, or a px or percent string)
+   */
+  top?: Number | string;
+  /**
+   * The right position of the counter (Number of pixels, or a px or percent string)
+   */
+  right?: Number | string;
+  /**
+   * The bottom position of the counter (Number of pixels, or a px or percent string)
+   */
+  bottom?: Number | string;
+  /**
+   * The left position of the counter (Number of pixels, or a px or percent string)
+   */
+  left?: Number | string;
+  /**
+   * The positioning to use. For example 'relative', 'absolute'
+   */
+  position?: string;
+}
+
+/**
+ * Representation of the current input position
+ */
+interface PositionParam {
+  top: Number;
+  right: Number;
+  bottom: Number;
+  left: Number;
+  width: Number;
+  height: Number;
+}
+
 import 'aureliatoolbelt-thirdparty/bootstrap-maxlength.js/bootstrap-maxlength.js';
 
 @containerless()
@@ -16,6 +62,48 @@ export class AureliaToolbeltMaxLength {
   public threshold: number = 10;
   @bindable({ defaultBindingMode: bindingMode.oneTime })
   public alwaysShow: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public warningClass: string = 'badge badge-success';
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public limitReachedClass: string = 'badge badge-danger';
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public separator: string = '/';
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public preText: string;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public postText: string;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public showMaxLength: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public showCharsTyped: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public placement:
+    | string
+    | MaxLengthPlacement
+    | PlacementOptions
+    | ((
+        currentInput: JQuery,
+        maxLengthIndicator: JQuery,
+        currentInputPosition: PositionParam
+      ) => void) = 'bottom';
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public appendToParent: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public message: string | ((currentText: string, maxLength: Number) => string);
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public utf8: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public showOnReady: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public twoCharLinebreak: boolean = true;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public customMaxAttribute: string;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public allowOverMax: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  public validate: boolean = false;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public shown: Function;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public hidden: Function;
 
   constructor(private element: HTMLInputElement) {}
   private isTextBox(element: Element) {
@@ -53,10 +141,40 @@ export class AureliaToolbeltMaxLength {
       throw Error('You should set `maxLength` property to your input element.');
     }
 
-    $(this.element).maxlength({
+    if (this.shown) {
+      $(this.element).on('maxlength.shown', () => {
+        if (this.shown) {
+          this.shown();
+        }
+      });
+    }
 
+    if (this.hidden) {
+      $(this.element).on('maxlength.hidden', () => {
+        if (this.shown) {
+          this.hidden();
+        }
+      });
+    }
+    $(this.element).maxlength({
       threshold: this.threshold,
-      alwaysShow: this.alwaysShow
+      alwaysShow: true,
+      warningClass: this.warningClass,
+      limitReachedClass: this.limitReachedClass,
+      separator: this.separator,
+      preText: this.preText,
+      postText: this.postText,
+      showMaxLength: this.showMaxLength,
+      showCharsTyped: this.showCharsTyped,
+      placement: this.placement,
+      appendToParent: this.appendToParent,
+      message: this.message,
+      utf8: this.utf8,
+      showOnReady: this.showOnReady,
+      twoCharLinebreak: this.twoCharLinebreak,
+      customMaxAttribute: this.customMaxAttribute,
+      allowOverMax: this.allowOverMax,
+      validate: this.validate
     });
   }
 }
