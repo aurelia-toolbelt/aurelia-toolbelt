96ea208bfb5427c4ac6e64ce5aa438cf0bccec34
Hamed Fathi <noeffect@outlook.com>
2019-02-03 14:40:33 +0330

    update: package.json
    Some parsers added.

---
+++ aurelia_project/tasks/doc.ts
@@ -0,0 +1,7 @@
+const path = require("path");
+const fs = require("fs");
+const tsdocparser = require("@microsoft/tsdoc");
+const jsdocparser = require("jsdoc3-parser");
+const htmlparser = require("htmlparser2");
+
+
